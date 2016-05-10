6a0bd7f924b49e0d5a9c9385c5e5a6680192e953
Ghanan Gowripalan <ghanan94@gmail.com>
2016-05-10 18:27:13 -0400

    Simple memory module that reads and writes 1 byte

--- Project 1/mem.sv
+++ Project 1/mem.sv
@@ -1,4 +1,19 @@
-module memory(output busy, output [31:0] data_out, input clk, read_write, enable, input[1:0] access_size, input [31:0] data_in, address);
+module memory_32bit(output reg busy, output reg [31:0] data_out, input wire clk, read_write, enable, input wire [1:0] access_size, input wire [31:0] data_in, address);
 
+	logic [31:0] memory [0:1048575];
+
+	always_ff @ (posedge clk) 
+	begin : MEM_READ
+		if (enable == 1) begin
+			case (read_write)
+				0 : memory[address] = data_in;
+				1 : data_out =  memory[address];
+			endcase
+		
+			busy = 1;
+		end
+		else 
+			busy = 0;
+	end
 endmodule
 
