19dbb5401ebad1e0e58f2c0e57438264c95f28b3
Ghanan Gowripalan <ghanan94@gmail.com>
2016-05-10 22:51:32 -0400

    Memory reading multiple words

--- Project 1/mem.sv
+++ Project 1/mem.sv
@@ -1,19 +1,65 @@
 module memory_32bit(output reg busy, output reg [31:0] data_out, input wire clk, read_write, enable, input wire [1:0] access_size, input wire [31:0] data_in, address);
 
-	logic [31:0] memory [0:1048575];
+	reg [7:0] memory [0:1048575];
+	reg reading;
+	reg [31:0] reading_address;
+	reg [3:0] read_cycles;
 
 	always_ff @ (posedge clk) 
 	begin : MEM_READ
-		if (enable == 1) begin
-			case (read_write)
-				0 : memory[address] = data_in;
-				1 : data_out =  memory[address];
-			endcase
-		
-			busy = 1;
+
+		if (enable) 
+		begin
+			if (read_write && ~reading) 
+			begin
+				reading <= 1;
+				reading_address <= address;
+
+				case (access_size)
+					00 : read_cycles <= 4'd0;
+					01 : read_cycles <= 4'd3;
+					10 : read_cycles <= 4'd7;
+					11 : read_cycles <= 4'd15;
+				endcase
+			end
+			else
+				reading <= 0;
 		end
-		else 
-			busy = 0;
+
+		if (reading) 
+		begin
+			data_out[7:0] <= memory[reading_address + 3];
+			data_out[15:8] <= memory[reading_address + 2];
+			data_out[23:16] <= memory[reading_address + 1];
+			data_out[31:24] <= memory[reading_address];
+
+			if (read_cycles == 4'd0)
+				reading <= 0;
+			else
+			begin
+				reading_address <= reading_address + 4;
+				read_cycles <= read_cycles - 1;
+			end
+		end
+	end
+
+	always_ff @ (posedge clk)
+	begin : MEM_WRITE
+		if (enable && ~read_write) 
+		begin
+			memory[address + 3] <= data_in[7:0];
+			memory[address + 2] <= data_in[15:8];
+			memory[address + 1] <= data_in[23:16];
+			memory[address] <= data_in[31:24];
+		end
+	end
+
+	always_ff @ (posedge clk)
+	begin : BUSY
+		if (enable || reading)
+			busy <= 1;
+		else
+			busy <= 0;
 	end
 endmodule
 
--- Project 1/mem_tb.sv
+++ Project 1/mem_tb.sv
@@ -9,12 +9,12 @@ module memory_32bit_tb;
 	logic busy;
 
 	initial begin
-		clk = 0;
+		clk = 1;
 		enable = 0;
 		read_write = 0;
 		data_in = 32'd0;
 		address = 32'd0;
-		access_size = 2'b0;
+		access_size = 2'd0;
 	end
 
 	always
@@ -29,12 +29,82 @@ module memory_32bit_tb;
 		#500 begin
 			enable = 1; 
 			read_write = 0;
+			access_size = 2'd0;
 			data_in = 32'd234;
 			address = 32'd0;
 		end
 
 		// Read from address 0 (should be value 234)
 		#500 begin 
+			read_write = 1;
+		end
+
+		// Write alue 1537628013 to address 4
+		#500 begin
+			read_write = 0;
+			data_in = 32'd1537628013;
+			address = 32'd4;
+		end
+
+		// Read from adress 1 (should be value 1537628013)
+		#500 begin
+			read_write = 1;
+		end
+
+		// Write alue 537628013 to address 8
+		#500 begin
+			read_write = 0;
+			data_in = 32'd537628013;
+			address = 32'd8;
+		end
+
+		// Read from adress 1 (should be value 537628013)
+		#500 begin
+			read_write = 1;
+		end
+
+		// Write alue 2537628013 to address 12
+		#500 begin
+			read_write = 0;
+			data_in = 32'd2537628013;
+			address = 32'd12;
+		end
+
+		// Read from adress 1 (should be value 2537628013)
+		#500 begin
+			read_write = 1;
+		end
+
+		// read from address 0 through 15 (should return 234, 1537628013, 537628013, 2537628013)
+		#500 begin
+			address = 32'd0;
+			access_size = 2'd1;
+		end
+
+		// Should have no effect
+		#1000 begin
+			enable = 0;
+		end
+
+		#2000 begin
+			enable = 1;
+			read_write = 0;
+			address = 32'd1048572;
+			data_in = 32'd10448573;
+			access_size = 2'd0;
+		end
+
+		#500 begin
+			read_write = 1;
+		end
+
+		#500 begin
+			read_write = 0;
+			address = 32'd1048576;
+			data_in = 32'd910448573;
+		end
+
+		#500 begin
 			read_write = 1;
 		end
 	end
