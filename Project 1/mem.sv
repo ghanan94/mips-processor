bf0c8bc82c2d0ea53c997cdcff875d38354b6200
Ghanan Gowripalan <ghanan94@gmail.com>
2016-05-13 19:23:26 -0400

    Memory stored with array of 32 bits instead of 8 bits

--- Project 1/mem.sv
+++ Project 1/mem.sv
@@ -1,4 +1,5 @@
 module memory #(
+	parameter benchmark = "",
 	parameter depth = 1048576
 )(
 	output reg busy, 
@@ -8,10 +9,15 @@ module memory #(
 	input wire [31:0] data_in, addr
 );
 
-	reg [7:0] memory [0:depth-1];
+	reg [31:0] memory [0:(depth >> 2)-1];
 	reg reading;
 	reg [31:0] reading_address;
 	reg [3:0] read_cycles;
+
+	initial
+	begin
+		$readmemh(benchmark, memory);
+	end
 
 	always_ff @ (posedge clk) 
 	begin : MEM_READ
@@ -21,7 +27,7 @@ module memory #(
 			if (rd_wr && ~reading) 
 			begin
 				reading <= 1;
-				reading_address <= addr;
+				reading_address <= addr >> 2;
 
 				case (access_size)
 					00 : read_cycles <= 4'd0;
@@ -36,16 +42,13 @@ module memory #(
 
 		if (reading) 
 		begin
-			data_out[7:0] <= memory[reading_address + 3];
-			data_out[15:8] <= memory[reading_address + 2];
-			data_out[23:16] <= memory[reading_address + 1];
-			data_out[31:24] <= memory[reading_address];
+			data_out <= memory[reading_address];
 
 			if (read_cycles == 4'd0)
 				reading <= 0;
 			else
 			begin
-				reading_address <= reading_address + 4;
+				reading_address <= reading_address + 1;
 				read_cycles <= read_cycles - 1;
 			end
 		end
@@ -55,10 +58,7 @@ module memory #(
 	begin : MEM_WRITE
 		if (enable && ~rd_wr) 
 		begin
-			memory[addr + 3] <= data_in[7:0];
-			memory[addr + 2] <= data_in[15:8];
-			memory[addr + 1] <= data_in[23:16];
-			memory[addr] <= data_in[31:24];
+			memory[addr >> 2] <= data_in;
 		end
 	end
 
