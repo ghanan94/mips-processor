d1701dff95f62cfb644641481c1c2cb073843781
Ghanan Gowripalan <ghanan94@gmail.com>
2016-05-21 14:12:17 -0400

    updated pd1_tb

--- Project 1/pd1_tb.sv
+++ Project 1/pd1_tb.sv
@@ -27,3 +27,35 @@ module tb_SimpleAdd;
     end
 
 endmodule
+
+
+/* tb_BubbleSort
+* Loads BubbleSort.x and tests reads of multiple words.
+*/
+module tb_BubbleSort;
+	logic clk, read, busy, en;
+	logic [31:0] addr, din, dout;
+	logic [1:0] size;
+
+    memory #(.benchmark("BubbleSort.x"), .depth(2**20)) mem(.clk(clk),
+		.addr(addr), .data_in(din), .data_out(dout),
+		.access_size(size), .rd_wr(read), .busy(busy), .enable(en));
+
+    initial $monitor("time %3d, addr %8h, data %8h, busy %1b", $time, addr,
+		dout, busy);
+ 
+    initial begin
+        clk = 0; forever #5 clk = ~clk;
+    end
+
+    initial begin
+        addr = 32'h80020000; size = 0; read = 1; en = 0;
+		#10 en = 1;
+		#10 size = 1;
+		#40 size = 2;
+		#80 size = 3;
+		#160 $stop;
+    end
+
+endmodule
+
