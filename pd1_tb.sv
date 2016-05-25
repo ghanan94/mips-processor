b50d8ac044734f7442b5e11d4323a5a5d1e300b6
Ghanan Gowripalan <ghanan94@gmail.com>
2016-05-25 14:33:43 -0400

    Moved pd1_tb.sv to root folder so the macro can load it

---
+++ pd1_tb.sv
@@ -0,0 +1,65 @@
+/* tb_SimpleAdd
+* Loads SimpleAdd.x and tests reads and writes of single words.
+*/
+module tb_SimpleAdd;
+	logic clk, read, busy, en;
+	logic [31:0] addr, din, dout;
+	logic [1:0] size;
+
+    memory #(.benchmark("SimpleAdd.x"), .depth(2**20)) mem(.clk(clk),
+		.addr(addr), .data_in(din), .data_out(dout),
+		.access_size(size), .rd_wr(read), .busy(busy), .enable(en));
+
+    initial $monitor("time %3d, addr %8h, data %8h, en %1b", $time, addr, dout,
+		en);
+ 
+    initial begin
+        clk = 1; forever #5 clk = ~clk;
+    end
+
+    initial begin
+        addr <= 32'h80020000; size <= 0; read <= 1; en <= 0;
+		#10 en <= 1;
+		#10 addr <= addr + 4;
+		#10 read <= 0; din <= 32'haaaaeeee;
+		#10 read <= 1;
+		#20 $stop; //$finish if using iverilog
+    end
+
+    //initial $dumpvars(0, tb_SimpleAdd); // for iverilog+gtkwave
+
+endmodule
+
+
+/* tb_BubbleSort
+* Loads BubbleSort.x and tests reads of multiple words.
+*/
+module tb_BubbleSort;
+	logic clk, read, busy, en;
+	logic [31:0] addr, din, dout;
+	logic [1:0] size;
+
+    memory #(.benchmark("BubbleSort.x"), .depth(2**20)) mem(.clk(clk),
+		.addr(addr), .data_in(din), .data_out(dout),
+		.access_size(size), .rd_wr(read), .busy(busy), .enable(en));
+
+    initial $monitor("time %3d, addr %8h, data %8h, busy %1b", $time, addr,
+		dout, busy);
+ 
+    initial begin
+        clk = 1; forever #5 clk = ~clk;
+    end
+
+    initial begin
+        addr <= 32'h80020000; size <= 0; read <= 1; en <= 0;
+		#10 en <= 1;
+		#10 size <= 1;
+		#40 size <= 2;
+		#80 size <= 3;
+		#160
+		#10 $stop; //$finish if using iverilog
+    end
+
+    //initial $dumpvars(0, tb_BubbleSort); // for iverilog+gtkwave
+	
+endmodule
